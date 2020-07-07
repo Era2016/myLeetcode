89bfd7a4895cc8853b35dbb13a14995c450c4164
zhangjiyuan <zhangjiyuan1@cmcm.com>
2020-07-07 09:01:48 +0800

    Unique Path

---
+++ Unique_Paths/Unique_Paths.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+/**
+If we use dp[i][j] for the number of unique paths to arrive at the point (i, j)
+then the state equation is dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+
+dp 数组初始化
+dp[0][j] = dp[i][0] = 1 for all valid i and j
+其余数组值可以是任何值，后面的计算会覆盖掉
+for i := 0; i < m; i++ {
+	for j := 0; j < n; j++ {
+		if i == 0 || j == 0 {
+			dp[i][j] = 1
+		} else {
+			dp[i][j] = 2
+		}
+	}
+}
+
+*/
+
+func uniquePaths(m int, n int) int {
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	// dp 数组初始化
+	// dp[0][j] = dp[i][0] = 1 for all valid i and j
+	// 其余数组值可以是任何值，后面的计算会覆盖掉
+
+	for j := 1; j < n; j++ {
+		dp[0][j] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		dp[i][0] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i][j-1] + dp[i-1][j]
+		}
+	}
+	return dp[m-1][n-1]
+}
+
+func main() {
+	m, n := 3, 2
+	fmt.Printf("m: %d, n: %d, result: %v\n", m, n, uniquePaths(m, n)) // 3
+
+	m, n = 7, 3
+	fmt.Printf("m: %d, n: %d, result: %v\n", m, n, uniquePaths(m, n)) // 28
+}
---
+++ Unique_Paths/solution.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int> > dp(m, vector<int>(n, 1));
+        //print(dp);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+    
+    void print(vector<vector<int> > vv) {
+        for (int i = 0; i < (int)vv.size(); i++) {
+            for (int j = 0; j < (int)vv[i].size(); j ++) {
+                cout << vv[i][j] << "\t";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+	//vector<int> dp = vector<int>(10, 1);
+	//for (int i = 0; i < (int)dp.size(); i ++) {
+	//	cout << dp[i] << "\t";
+	//}
+	//cout << endl;
+	Solution* so = new Solution();
+    //so->uniquePaths(7, 3);
+	cout << "(3, 2) result: " << so->uniquePaths(3, 2) << endl;
+	cout << "(7, 3) result: " << so->uniquePaths(7, 3) << endl;
+}
