c418025a1354350b36ea8c9d03e39b601ad9dec9
zhangjiyuan <zhangjiyuan1@cmcm.com>
2020-07-24 09:31:32 +0800

    update

--- Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/ConstructBinaryTree.cpp
+++ Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/ConstructBinaryTree.cpp
@@ -20,26 +20,45 @@ public:
     		return NULL;
     	}
 
-    	int i = 0;
-    	while (i < (int)inorder.size() && inorder[i] != preorder[0]) {
-    		i ++;
-    	}
-
-    	// 数据异常
-    	if (i == (int)inorder.size()) {
-    		return NULL;
-    	}
-
- 		TreeNode* node = new TreeNode();
- 		node->val = preorder[0]
- 		// 构建左子树
- 		node->left = construct(preorder, 0, i - 1, inorder, );
- 		// 构建右子树
- 		node->right = construct();
+    	return construct(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1);
     }
 private:
 	TreeNode* construct(vector<int>& preorder, int startP, int endP, 
 		vector<int>& inorder, int startI, int endI) {
 
+		if (startP == endP && startI == endI) {
+			return NULL;
+		}
+
+    	// 左子树数据范围确认
+		int i = startI;
+    	while (i < (int)inorder.size() && inorder[i] != preorder[startP]) {
+    		i ++;
+    	}
+
+    	// 数据异常，前序中序数据不一致
+    	if (i == (int)inorder.size()) {
+    		return NULL;
+    	}
+
+    	int counter = i - startI + 1;
+
+    	// 右子树数据范围确认
+
+		// counter >= 1
+    	if (counter < 1) {
+    		return NULL;
+    	}
+
+    	// startP+counter+1 <= enp
+    	// startI+counter <= endI
+
+ 		TreeNode* node = new TreeNode();
+ 		node->val = preorder[startP]
+ 		// 构建左子树
+ 		node->left = construct(preorder, startP+1, startP+counter, inorder, startI, startI+counter-1);
+ 		// 构建右子树
+ 		node->right = construct(preorder, startP+counter+1, endP, inorder, startI+counter, endI);
+ 		return node;
 	}
 };
