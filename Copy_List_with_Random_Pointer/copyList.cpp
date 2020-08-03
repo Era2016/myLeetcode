ecf079cc9d01da8f71f80b1f2b6c755d109e650b
zhangjiyuan <zhangjiyuan1@cmcm.com>
2020-08-03 11:30:49 +0800

    update

--- Copy_List_with_Random_Pointer/copyList.cpp
+++ Copy_List_with_Random_Pointer/copyList.cpp
@@ -23,10 +23,12 @@ public:
     	// new and connect
     	nodeConnect(head);
     	//print(head);
+        //cout << "nodeConnect finished" << endl;
 
     	// random pointer copy
     	pointerCopy(head);
     	//print(head);
+        //cout << "pointerCopy finished" << endl;
 
     	// link decoupling
     	return decoupling(head);
@@ -53,21 +55,28 @@ private:
 	}
 
 	Node* decoupling(Node* head) {
+        if (head == NULL) {
+            return NULL;
+        }
+
 		Node* p = head->next;
 		Node* newHead = p;
 		while (head != NULL) {
 			Node* tmp = p->next;
 
-			head->next = tmp;
-			p->next = tmp->next;
+            if (tmp == NULL) {
+                head->next = NULL;
+                p->next = NULL; 
+                break;
+            } else {
+                head->next = tmp;
+                p->next = tmp->next;
+            }
 
 			head = tmp;
-			if (head == NULL) {
-				break;
-			}
 			p = tmp->next;
 		}
-		print(newHead);
+		//print(newHead);
 		return newHead;
 	}
 };
@@ -82,12 +91,19 @@ void print(Node* head) {
 }
 
 int main() {
-	Node* node1 = new Node(1);
+	/*Node* node1 = new Node(1);
 	Node* node2 = new Node(2);
-	node1->next = node2;
-	node1->random = node2;
-	node2->random = node2;
+	node1->next = node2; node1->random = node2;
+	node2->random = node2;*/
 
+    /*Node* node1 = new Node(3);
+    Node* node2 = new Node(3);
+    Node* node3 = new Node(3);
+    node1->next = node2; node1->random = NULL;
+    node2->next = node3; node2->random = node1;
+    node3->random = NULL;*/
+
+    Node* node1 = NULL;// = new Node(1);
 	print(node1);
 	Solution* so = new Solution();
 	Node* head = so->copyRandomList(node1);
