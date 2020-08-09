15adfa250dd9a3a238cdbd9082f246bc0134d62d
zhangjiyuan <zhangjiyuan1@cmcm.com>
2020-08-09 23:45:36 +0800

    update

--- Max_Points_on_a_Line/Max_Points_on_a_Line.go
+++ Max_Points_on_a_Line/Max_Points_on_a_Line.go
@@ -1,9 +1,15 @@
 package main
 
 import "fmt"
+import "strings"
+import "strconv"
 
-// y = kx + b
-func maxPoints(points [][]int) int {
+/**
+* 重复点当做1
+* y = kx + b
+  k 有精度问题
+ */
+func maxPointsWithoutRepeated(points [][]int) int {
 
 	if len(points) == 0 {
 		return 0
@@ -31,17 +37,71 @@ func maxPoints(points [][]int) int {
 				}
 			} else {
 				//fmt.Printf("current (%v,%v)\n", i, j)
-                m[key][i] = true
-                m[key][j] = true
+				m[key][i] = true
+				m[key][j] = true
 			}
 		}
 	}
 
-	//fmt.Println(m)
 	for _, v := range m {
-		//fmt.Println(v)
 		if max < len(v) {
 			max = len(v)
+		}
+	}
+	return max
+}
+
+// 有精度问题
+func maxPoints(points [][]int) int {
+
+	if len(points) <= 1 {
+		return len(points)
+	}
+
+	m := make(map[string]bool)
+
+	for i := 0; i < len(points)-1; i++ {
+		for j := i + 1; j < len(points); j++ {
+			minus, k := points[j][0]-points[i][0], 0
+			var key string
+			if minus == 0 {
+				key = fmt.Sprintf("#%d", points[i][1])
+			} else {
+				k = (points[j][1] - points[i][1]) / minus
+				b := points[j][1] - k*points[j][0]
+				key = fmt.Sprintf("%d#%d", k, b)
+			}
+
+			if _, ok := m[key]; !ok {
+				m[key] = true
+			}
+		}
+	}
+
+	fmt.Println(m)
+	var x, k, b int64
+	var max int
+	for v, _ := range m {
+		ret := strings.Split(v, "#")
+		tmpMax := 0
+		for i := 0; i < len(points); i++ {
+			if ret[0] == "" {
+				x, _ = strconv.ParseInt(ret[1], 10, 64)
+				if points[i][0] == int(x) {
+					tmpMax++
+				}
+			} else {
+				k, _ = strconv.ParseInt(ret[0], 10, 64)
+				b, _ = strconv.ParseInt(ret[1], 10, 64)
+				if points[i][1] == int(k)*points[i][0]+int(b) {
+					tmpMax++
+				}
+			}
+
+			if max < tmpMax {
+				max = tmpMax
+			}
+
 		}
 	}
 	return max
--- Max_Points_on_a_Line/pointOnLine.cpp
+++ Max_Points_on_a_Line/pointOnLine.cpp
@@ -3,14 +3,37 @@
 
 using namespace std;
 
+struct Point
+{
+	x, y int
+};
 
 class Solution {
 public:
-    int maxPoints(vector<vector<int> >& points) {
-        
+    int maxPoints(vector<Point> &points) {
+    int result = 0;
+    for(int i = 0; i < points.size(); i++){
+        int samePoint = 1;
+        unordered_map<double, int> map;
+        for(int j = i + 1; j < points.size(); j++){
+            if(points[i].x == points[j].x && points[i].y == points[j].y){
+                samePoint++;
+            }
+            else if(points[i].x == points[j].x){
+                map[INT_MAX]++;
+            }
+            else{
+                double slope = double(points[i].y - points[j].y) / double(points[i].x - points[j].x);
+                map[slope]++;
+            }
+        }
+        int localMax = 0;
+        for(auto it = map.begin(); it != map.end(); it++){
+            localMax = max(localMax, it->second);
+        }
+        localMax += samePoint;
+        result = max(result, localMax);
     }
-
-	bool constructLine(vector<vector<int> > v) {
-		
+    return result;
 	}
 };
