13e3ebb8f17c46e5a7f2e91cffd697f88921effb
zhangjiyuan <zhangjiyuan1@cmcm.com>
2020-09-23 10:15:37 +0800

    update

--- Word_Break/solution.cpp
+++ Word_Break/solution.cpp
@@ -8,7 +8,7 @@ using namespace std;
 class Solution {
 public:
     // time out: 2的n次方复杂度
-    bool wordBreak2(string s, vector<string>& wordDict) {
+    bool wordBreak1(string s, vector<string>& wordDict) {
 		if (s.length() == 0) {
 			return true;
 		}
@@ -30,7 +30,8 @@ public:
 		return false;
     }
 
-    bool wordBreak(string s, vector<string>& wordDict) {
+    // before optimize
+    bool wordBreak2(string s, vector<string>& wordDict) {
         unordered_set<string> uset(wordDict.begin(), wordDict.end()); 
         vector<bool> dp(s.length()+1, false);
         dp[0] = true;
@@ -49,6 +50,37 @@ public:
         }
         return dp[s.length()];
     }
+
+    // after optimize: 
+    // 减少查询次数，以字典中最长word为准；
+    // 优先判断之前计算结果，然后再查找
+    bool wordBreak3(string s, vector<string>& wordDict) {
+        unordered_set<string> uset(wordDict.begin(), wordDict.end()); 
+        int longestWord = 0;
+        for (string word: uset) {
+            longestWord = max(longestWord, (int)word.length()); 
+        }
+
+        vector<bool> dp(s.length()+1, false);
+        dp[0] = true;
+
+        for (int i = 1; i <= (int)s.length(); i ++) {
+            for (int j = i-1; j >= max(0, i-longestWord); j --) {
+                if (dp[j] == true) {
+                    string slice = s.substr(j, i-j);
+                    //cout << "i: " << i << " j: " << j << " slice: " << slice << endl;
+                    unordered_set<string>::iterator iter = uset.find(slice);
+                    if (iter != uset.end()) {
+                        dp[i] = true; 
+                        //cout << "index: " << i << " flag: " << j << " slice: " << slice << endl;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
 };
 
 int main() {
@@ -56,16 +88,16 @@ int main() {
 	vector<string> v = {"leet", "code"};
 	bool val = true;
 	string str = "leetcode";
-	val = so->wordBreak(str, v);
+	val = so->wordBreak3(str, v);
 	cout << val << endl << endl;
 
     v = {"apple", "pen"};
     str = "applepenapple";
-	val = so->wordBreak(str, v);
+	val = so->wordBreak3(str, v);
 	cout << val << endl << endl;
 
     v = {"cats", "dog", "sand", "and", "cat"};
     str = "catsandog";
-	val = so->wordBreak(str, v);
+	val = so->wordBreak3(str, v);
 	cout << val << endl << endl;
 }
