77eab40748f1a6bb6e9d28e6d6274a459fe48700
zhangjiyuan <zhangjiyuan1@cmcm.com>
2020-10-11 11:10:14 +0800

    update

--- Perfect_Squares/solution.cpp
+++ Perfect_Squares/solution.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <chrono> // Measure execution time of a function in C++ 
+#include <chrono>     // Measure execution time of a function in C++ 
+#include <stdlib.h>   // rand
 
 using namespace std;
 using namespace std::chrono;
@@ -24,8 +25,7 @@ public:
         return dp[n];
     }
 
-	int numSquares2(int n) 
-    {
+	int numSquares2(int n) {
         if (n <= 0) {
             return 0;
         }
@@ -34,6 +34,7 @@ public:
         // which sum to i. Since cntPerfectSquares is a static vector, if 
         // cntPerfectSquares.size() > n, we have already calculated the result 
         // during previous function calls and we can just return the result now.
+        //
         static vector<int> cntPerfectSquares({0});
         
         // While cntPerfectSquares.size() <= n, we need to incrementally 
@@ -41,8 +42,8 @@ public:
         while ((int)cntPerfectSquares.size() <= n) {
             int m = cntPerfectSquares.size();
             int cntSquares = INT_MAX;
-            for (int i = 1; i*i <= m; i++) {
-                cntSquares = min(cntSquares, cntPerfectSquares[m - i*i] + 1);
+            for (int j = 1; j*j <= m; j ++) {
+                cntSquares = min(cntSquares, cntPerfectSquares[m - j*j] + 1);
             }
             
             cntPerfectSquares.push_back(cntSquares);
@@ -54,22 +55,29 @@ public:
 
 int main() {
     Solution* so = new Solution();
-    cout << "first =====" << endl;
-    for (int i = 0; i < 10; i ++) {
-        auto start = high_resolution_clock::now();
-        int val = so->numSquares(123456 + i);
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<microseconds>(stop - start);
-        cout << val << " consuming:" << duration.count() << endl; 
+    srand (time(NULL));
+
+    vector<int> v;
+    for (int i = 0; i < 100; i ++) {
+        v.push_back(rand() % 100000 + 10000);
     }
 
-    cout << "second=====" << endl;
-    for (int i = 0; i < 10; i ++) {
+    //cout << "first =====" << endl;
+    for (int i = 0; i < (int)v.size(); i ++) {
+        auto start = high_resolution_clock::now();
+        int val = so->numSquares(v[i]);
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        cout << "origin:" << v[i] << ", result: " <<val << "; first consuming:" << duration.count() << " ms."<< endl; 
+    //}
+
+    //cout << "second=====" << endl;
+    //for (int i = 0; i < 10; i ++) {
         auto start2 = high_resolution_clock::now();
-        int val2 = so->numSquares2(123456 + i);
+        int val2 = so->numSquares2(v[i]);
         auto stop2 = high_resolution_clock::now();
         auto duration2 = duration_cast<microseconds>(stop2 - start2);
-        cout << val2 << " consuming:" << duration2.count() << endl; 
+        cout << "origin:" << v[i] << ", result: " <<val2 << "; second consuming:" << duration2.count() << " ms."<< endl; 
     }
 
     //val = so->numSquares(13);
