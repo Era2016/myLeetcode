fa2f3e2e3edb1d4bb57aaec0d384058f3c585c8e
zhangjiyuan <zhangjiyuan1@cmcm.com>
2020-10-29 16:16:26 +0800

    updat

---
+++ Binary_Search_Tree_Iterator/solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class BSTIterator {
+public:
+    BSTIterator(TreeNode* root) {
+        
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
