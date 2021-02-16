05d46adc043a99bb30ef5739b1baffd63be84a14
zhangjiyuan <zhangji_yuan@yahoo.com>
2021-02-16 22:05:52 +0800

    update  Convert_Sorted_Array_to_Binary_Search_Tree

--- Convert_Sorted_Array_to_Binary_Search_Tree/solution.cpp
+++ Convert_Sorted_Array_to_Binary_Search_Tree/solution.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -46,4 +47,24 @@ public:
 		root->right = help(nums, mid+1, end);
 		return root;
 	}
+
+    void BSTPrint(TreeNode* root) {
+        if (root == nullptr) return;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front(); q.pop();
+            cout << node->val << "\t";
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
+        }
+    }
 };
+
+int main() {
+    vector<int> v = {-10,-3,0,5,9};
+    Solution* so = new Solution();
+    TreeNode* node = so->sortedArrayToBST(v); 
+    so->BSTPrint(node);
+}
