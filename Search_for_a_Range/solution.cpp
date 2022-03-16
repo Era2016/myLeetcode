8a96e2e52f4a2276989b70451af5d71d6e80f764
zhangjiyuan <zhangjiyuan1@joyme.sg>
2022-03-16 19:57:44 +0800

    update

--- Search_for_a_Range/solution.cpp
+++ Search_for_a_Range/solution.cpp
@@ -3,77 +3,50 @@
 
 using namespace std;
 
+void print(vector<int>);
 class Solution {
-    public:
-        vector<int> searchRange(vector<int>& nums, int target) {
-            int length = nums.size();
-            int start = 0;
-            int end = length - 1;
-            int index = -1;
-            vector<int> v;
-            while (start <= end) {
-                int mid = (start + end) / 2;
-                if (nums[mid] > target) {
-                    end = mid - 1;
-                } else if (nums[mid] < target) {
-                    start = mid + 1;
-                } else {
-                    index = mid;
-                    cout << index << endl;
-                    while (true) {
-                        if (index - 1 > -1) {
-                            if (nums[index - 1] == target) {
-                                index --;
-                            } else {
-                                break;
-                            }
-                        } else {
-                            break;
-                        }
-                    } 
-                    cout << index << endl;
-                    v.push_back(index);
-                    index = mid;
-                    while (true) {
-                        if (index + 1 < length) {
-                            if (nums[index + 1] == target) {
-                                index ++;
-                            } else {
-                                break;
-                            }
-                        } else {
-                            break;
-                        }
-                    }
-                    cout << index << endl;
-                    v.push_back(index);
-                    return v;
-                }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int length = nums.size();
+        int mid, left = 0, right = length - 1; 
+        bool find = false;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (target < nums[mid]) {
+                right = mid - 1; 
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                find = true;
+                break;
             }
-            v.push_back(-1);
-            v.push_back(-1);
-            return v;
         }
+
+        if (!find) return {-1, -1};
+
+        int min = mid, max = mid;
+        while (min > 0 && nums[min-1] == target) 
+            min --;
+        while (max < length-1 && nums[max+1] == target)
+            max ++;
+
+        return {min, max};
+    }
 };
 
-int main()
-{
-    Solution* so = new Solution();
-    vector<int> v;
-    while (true) {
-        int temp;
-        cin >> temp;
-        if (temp != -1) {
-            v.push_back(temp);
-        } else {
-            cout << "input done" << endl;
-            break;
-        }
-    }
-    int target;
-    cin >> target;
+void print(vector<int> v) {
+    for (vector<int>::iterator it = v.begin(); it != v.end(); it ++)
+        cout << *it << "\t";
+    cout << endl;
+}
 
+int main() {
+    vector<int> v = {5,7,7,8,8,10};
+    //vector<int> v = {5,7,7,8,8,10};
+    //vector<int> v = {};
+    int target = 8;
+   
+    Solution* so = new Solution();
     vector<int> ret = so->searchRange(v, target);
-    cout << ret[0] << "\t" << ret[1] << endl;
-    return 0;
+    print(ret);
 }
