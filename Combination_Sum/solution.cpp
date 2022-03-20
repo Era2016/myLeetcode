20ce25f2fbcd7d81a15ee3709aae8eff0169107c
zhangjiyuan <zhangjiyuan1@joyme.sg>
2022-03-20 16:19:14 +0800

    update

--- Combination_Sum/solution.cpp
+++ Combination_Sum/solution.cpp
@@ -4,46 +4,62 @@
 using namespace std;
 
 class Solution {
-    public:
-        vector<vector<int> > combinationSum(vector<int>& candidates, int target) {
-            vector<vector<int> > res;
-            vector<int> out;
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> combine;
+        dfs(candidates, target, ans, combine, 0);
+        return ans;
+    }
 
-            sort(candidates.begin(), candidates.end());
-            combinationSumDFS(candidates, target, 0, out, res);
-            return res;
+    void dfs(vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& combine, int idx) {
+        if (idx == candidates.size()) {
+            return;
         }
+        if (target == 0) {
+            ans.emplace_back(combine);
+            return;
+        }
+        // 直接跳过
+        dfs(candidates, target, ans, combine, idx + 1);
+        // 选择当前数
+        if (target - candidates[idx] >= 0) {
+            combine.emplace_back(candidates[idx]);
+            dfs(candidates, target - candidates[idx], ans, combine, idx);
+            combine.pop_back();
+        }
+    }
 
-        void combinationSumDFS(vector<int>& candidates, int target, int start, vector<int>& out, vector<vector<int> >& res) {
-            if (target < 0) 
-                return ;
-            else if (target == 0) {
-                res.push_back(out);
-            } else {
-                for (int i = start; i < (int)candidates.size(); ++ i) {
-                    out.push_back(candidates[i]);
-                    combinationSumDFS(candidates, target - candidates[i], i, out, res);
-                    out.pop_back();
-                }
-            }
-        }
 };
 
-int main()
-{
-    Solution* so = new Solution();
-    vector<int> v;
-    v.push_back(2);
-    v.push_back(3);
-    v.push_back(6);
-    v.push_back(7);
-
-    vector<vector<int> >vv = so->combinationSum(v, 7);
+void print(vector<vector<int>> vv) {
     for (vector<vector<int> >::iterator iter = vv.begin(); iter != vv.end(); ++ iter) {
         for (vector<int>::iterator it = iter->begin(); it != iter->end(); ++ it) {
             cout << *it << "\t";
         }
         cout << endl;
     }
-    return 0;
+    cout << endl;
+}
+
+void print(vector<int> v) {
+    for (vector<int>::iterator it = v.begin(); it != v.end(); ++ it)
+        cout << *it << "\t";
+    cout << endl;
+}
+
+int main() {
+    Solution* so = new Solution();
+    vector<vector<int>> vv;
+
+    //vector<int> v = {2,3,6,7};
+    //int target = 7;
+
+    vector<int> v = {2,3,5};
+    int target = 8;
+
+    //vector<int> v = {2};
+    //int target = 1;
+    vv = so->combinationSum(v, target);
+    print(vv);
 }
