62c7c899b83dbc29928f44c18588137cfb9ea93f
zhangjiyuan <zhangjiyuan1@joyme.sg>
2022-04-23 17:39:51 +0800

    update solution for subsets
    Backtracking

--- Subsets/solution.cpp
+++ Subsets/solution.cpp
@@ -1,41 +1,42 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;;
+using namespace std;
 
 class Solution {
 public:
-    vector<vector<int> > subsets(vector<int>& nums) {
-        vector<vector<int> > subs;
-        vector<int> sub;
-        subsets(nums, 0, sub, subs);
-        return subs;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> vv;
+        vector<int> v;
+
+        dfs(nums, vv, v, 0);
+        return vv;
     }
 
-private:
-    void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int> >& subs) {
-        subs.push_back(sub);
-        for (int j = i; j < (int)nums.size(); j++) {
-            sub.push_back(nums[j]);
-            subsets(nums, j + 1, sub, subs);
-            sub.pop_back();
+    void dfs(vector<int>& nums, vector<vector<int>>& vv, vector<int>& v, int idx) {
+        vv.push_back(v);
+
+        for (int i = idx; i < nums.size(); i ++) {
+            v.push_back(nums[i]);
+            dfs(nums, vv, v, i+1);
+            v.pop_back();
         }
     }
 };
 
-int main() {
-    vector<int> v;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    vector<vector<int> > vv;
-    Solution* so = new Solution();
-    vv = so->subsets(v);
-
-    for (vector<vector<int> >::iterator iter = vv.begin(); iter != vv.end(); ++ iter) {
-        for (vector<int>::iterator it = iter->begin(); it != iter->end(); ++ it) {
+void print(vector<vector<int>> vv) {
+    for (vector<vector<int>>::iterator iter = vv.begin(); iter != vv.end(); iter ++) {
+        for (vector<int>::iterator it = (*iter).begin(); it != (*iter).end(); it ++) {
             cout << *it << "\t";
         }
         cout << endl;
     }
+    cout << endl;
+}
+
+int main() {
+    vector<int> v = {1,2,3};
+    Solution* so = new Solution();
+    vector<vector<int>> vv = so->subsets(v);
+    print(vv);
 }
