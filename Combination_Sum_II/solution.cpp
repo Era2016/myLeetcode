21c3b90ae71f11ec4d2032798f8e9e7dae0f8616
zhangjiyuan <zhangjiyuan1@joyme.sg>
2022-04-23 16:34:08 +0800

    update

--- Combination_Sum_II/solution.cpp
+++ Combination_Sum_II/solution.cpp
@@ -1,67 +1,89 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 
 using namespace std;
+
+void print(vector<pair<int, int>>& v);
+void print(vector<vector<int>>& v);
 
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<pair<int, int>> source;
+        for (int i = 0; i < candidates.size(); i ++) {
+            if (source.empty() || source.back().first != candidates[i]) {
+                source.push_back(pair<int, int>(candidates[i], 1));
+            } else {
+                source.back().second ++;
+            }
+        }
+
+        //print(source);
+
         vector<int> v;
         vector<vector<int>> vv;
-        sort(candidates.begin(), candidates.end());
+        dfs(source, vv, v, target, 0);
 
-        dfs(candidates, target, vv, v, 0);
         return vv;
     }
 
-    void dfs(vector<int>& candidates, int target, vector<vector<int>>& vv, vector<int>& v, int idx) {
-        if (target < 0) {
-            return;
-        }
+    void dfs(vector<pair<int, int>>& source, vector<vector<int>>& vv, 
+            vector<int>& v, int target, int depth) {
 
         if (target == 0) {
             vv.push_back(v);
-            return;
-        } 
+            return ;
+        }
 
-        for (int i = idx; i < candidates.size(); i ++) {
-            if (i > idx && candidates[i-1] == candidates[i]) 
-                continue;
-            v.push_back(candidates[i]);
-            dfs(candidates, target-candidates[i], vv, v, i+1);
+        if (depth == source.size() || target < 0) {
+            return ;
+        }
+
+        
+        dfs(source, vv, v, target, depth+1);
+
+        int most = min(target/source[depth].first, source[depth].second);
+        for (int i = 1; i <= most; i ++) {
+            //cout << "input stack: " << source[depth].first << endl;
+            v.push_back(source[depth].first);
+            dfs(source, vv, v, target - i*source[depth].first, depth+1);
+        }
+        
+        for (int i = 1; i <= most; i ++) {
+            //cout << "output stack: " << v.back() << endl;
             v.pop_back();
         }
     }
 };
 
-void print(vector<vector<int>> vv) {
-    for (vector<vector<int>>::iterator it = vv.begin(); it != vv.end(); it ++) {
-        for (vector<int>::iterator i = (*it).begin(); i != (*it).end(); i ++) {
-            cout << *i << "\t";
+void print(vector<pair<int, int>>& v) {
+    for (vector<pair<int, int>>::iterator iter = v.begin(); iter != v.end(); iter ++) {
+        cout << (*iter).first << " -> " << (*iter).second << endl;
+    }
+    cout << endl;
+}
+
+void print(vector<vector<int>>& vv) {
+    for (vector<vector<int>>::iterator iter = vv.begin(); iter != vv.end(); iter ++) {
+        for (vector<int>::iterator it = (*iter).begin(); it != (*iter).end(); it ++) {
+            cout << *it << "\t";
         }
         cout << endl;
     }
     cout << endl;
 }
 
-void print(vector<int> v) {
-    for (vector<int>::iterator it = v.begin(); it != v.end(); it ++)
-        cout << *it << "\t";
-    cout << endl;
-}
-
 int main() {
-    vector<int> v = {5,2,3,1};
-    int target = 8;
-    
-    //vector<int> v = {10,1,2,7,6,1,5};
-    //int target = 8; 
+    //vector<int> v =  {10,1,2,7,6,1,5};
+    //int target = 8;
 
-    //vector<int> v = {2,5,2,1,2};
-    //int target = 5;
+    vector<int> v = {2,5,2,1,2};
+    int target = 5;
 
-    vector<vector<int>> vv;
     Solution* so = new Solution();
-    vv = so->combinationSum2(v, target);
-    print(vv);
+    vector<vector<int>> vv = so->combinationSum2(v, target); 
+    print(vv); 
 }
+
