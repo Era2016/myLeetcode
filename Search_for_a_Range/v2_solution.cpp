314c676dcca18c35e4de17249a068bcb4c397e44
zhangjiyuan <zhangjiyuan1@joyme.sg>
2022-10-25 20:40:34 +0800

    [Search for a Range] add added a solution

---
+++ Search_for_a_Range/v2_solution.cpp
@@ -0,0 +1,25 @@
+#include <random>
+#include <type_traits>
+#include <vector>
+
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::vector;
+using std::random_device;
+
+class Solution {
+private:
+    mt19937 gen;
+    uniform_int_distribution<int> dis;
+    vector<int> pre;
+
+public:
+    Solution(vector<int>& w): gen(random_device{}()), dis(1, accumulate(w.begin(), w.end(), 0)) {
+        partial_sum(w.begin(), w.end(), back_inserter(pre));
+    }
+    
+    int pickIndex() {
+        int x = dis(gen);
+        return lower_bound(pre.begin(), pre.end(), x) - pre.begin();
+    }
+};
