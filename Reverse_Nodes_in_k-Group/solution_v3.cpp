0896fee4ac89cc123b972c7cfa345c65a2c9f13c
zhangjiyuan <zhangjiyuan1@joyme.sg>
2023-11-09 14:38:27 +0800

    update

---
+++ Reverse_Nodes_in_k-Group/solution_v3.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include "../utils/listNode.h"
+//struct ListNode {
+//    int val;
+//    ListNode *next;
+//    ListNode(): val(0), next(nullptr) {};
+//    ListNode(int val): val(val), next(nullptr) {};
+//    ListNode(int val, ListNode *next): val(val), next(next) {};
+//};
+
+using std::vector;
+class Solution {
+private:
+    ListNode* reverseBetween(ListNode *head, int left, int right) {
+        ListNode *dummy = new ListNode(0, head);
+        ListNode *prev = dummy;
+        for (int i = 0; i < left - 1; i ++) {
+            prev = prev->next;
+        }
+
+        ListNode *cur = prev->next;
+        ListNode *next;
+        int cnt = 0;
+        while (cnt < right-left) {
+            next = cur->next;
+
+            cur->next = next->next;
+            next->next = prev->next;
+            prev->next = next;
+            cnt ++;
+        }
+        return dummy->next;
+    }
+
+public:
+    ListNode* reverseKGroup(ListNode *head, int k) {
+        ListNode *dummy = new ListNode();
+        dummy->next = head;
+        ListNode *prev = dummy;
+        ListNode *tail, *cur, *next;
+        tail = cur = head;
+        int index = 0;
+        while (tail != nullptr) {
+            for (int i = 1; i < k && tail != nullptr; i ++) {
+                index ++;
+                tail = tail->next; 
+            }
+            if (tail == nullptr) {
+                return dummy->next;
+            }
+
+            next = tail->next; 
+            prev->next = reverseBetween(cur, 1, k);
+            cur->next = next;
+
+            prev = cur;
+            cur = prev->next;
+            tail = cur;
+            index ++;
+        }
+        return dummy->next;
+    }
+};
+
+int main() {
+    Solution *so = new Solution();
+    vector<int> v;
+    ListNode *head;
+
+    v = {1,2,3,4,5,6};
+    head = createList(v);
+    showList(head);
+    head = so->reverseKGroup(head, 2);
+    showList(head);
+
+    v = {1,2,3,4,5,6,7};
+    head = createList(v);
+    showList(head);
+    head = so->reverseKGroup(head, 3);
+    showList(head);
+
+    v = {1,2,3,4,5};
+    head = createList(v);
+    showList(head);
+    head = so->reverseKGroup(head, 2);
+    showList(head);
+
+    v = {1,2};
+    head = createList(v);
+    showList(head);
+    head = so->reverseKGroup(head, 2);
+    showList(head);
+}
